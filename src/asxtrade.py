b47761584ebf7edf09909add8cbca7fc7ce3b088
Andrew Cassin <acassin999@gmail.com>
2020-07-06 14:15:36 +1000

    Add prices and company details to supported API endpoints from ASX

--- src/asxtrade.py
+++ src/asxtrade.py
@@ -2,13 +2,24 @@
 import pymongo
 import argparse
 import requests
-from datetime import datetime
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+from datetime import datetime, timedelta, timezone
 import json
 import csv
 import olefile
 import tempfile
+import time
 import pandas as pd
 import os
+
+retry_strategy = Retry(
+    total=10,
+    status_forcelist=[429, 500, 502, 503, 504],
+    method_whitelist=["HEAD", "GET", "OPTIONS"],
+    backoff_factor=5
+)
+retry_adapter = HTTPAdapter(max_retries=retry_strategy)
  
 def update_companies(db, config, ensure_indexes=True):
     resp = requests.get(config.get('asx_companies'))
@@ -62,12 +73,86 @@ def update_isin(db, config, ensure_indexes=True):
              db.asx_isin.find_one_and_update({ 'asx_isin_code': isin_code, 'asx_code': asx_code }, { "$set": d }, upsert=True)
              n += 1
          print("Saved {} securities to {} for validation by great_expectations.".format(n, fname))
-          
+
+def get_fetcher():
+    fetcher = requests.Session()
+    fetcher.mount("https://", retry_adapter)
+    fetcher.mount("http://", retry_adapter)
+    return fetcher
+
+def update_prices(db, available_stocks, config, ensure_indexes=True):
+    assert isinstance(config, dict)
+    assert len(available_stocks) > 1000
+  
+    if ensure_indexes:
+        db.asx_prices.create_index([('asx_code', pymongo.ASCENDING), ('fetch_date', pymongo.ASCENDING)], unique=True)
+
+    fetcher = get_fetcher()
+    df = None 
+    fetch_date = datetime.now().strftime("%Y-%m-%d")
+    for asx_code in available_stocks:
+        url = "{}{}{}".format(config.get('asx_prices'), '' if config.get('asx_prices').endswith('/') else '/', asx_code)
+        print("Fetching {} prices from {}".format(asx_code, url))
+        resp = fetcher.get(url)
+        d = json.loads(resp.content.decode())
+        d.update({ 'fetch_date': fetch_date })
+        #assert len(d.keys()) > 10
+        time.sleep(5)  # be nice to the API endpoint
+        if df is None:
+            df = pd.DataFrame(columns=d.keys()) 
+        row = pd.Series(d, name=asx_code)
+        df.append(row)
+        db.asx_prices.find_one_and_update({ 'asx_code': asx_code, 'fetch_date': d.get('fetch_date') }, { '$set': d }, upsert=True)
+    fname = "{}/asx_prices.{}.tsv".format(config.get('data_root'), fetch_date)
+    df.to_csv(fname, sep='\t')
+    print("Saved {} stock codes with prices to {}".format(len(df), fname))
+   
+def available_stocks(db):
+    ret = set([r.get('asx_code') for r in db.asx_isin.find({ 'security_name': 'ORDINARY FULLY PAID' })])
+    print("Found {} stocks on ASX...".format(len(ret)))
+    return ret
+
+def update_company_details(db, available_stocks, config, ensure_indexes=False):
+    assert len(available_stocks) > 1000
+    assert db is not None
+    assert isinstance(config, dict)
+    fetcher = get_fetcher() 
+
+    if ensure_indexes:
+        db.asx_company_details.create_index([ ('asx_code', pymongo.ASCENDING, ), ], unique=True)
+
+    for asx_code in available_stocks:
+        url = config.get('asx_company_details')
+        url = url.replace('%s', asx_code)
+        print(url)
+        rec = db.asx_company_details.find_one({ 'asx_code': asx_code })
+        if rec is not None:
+            dt = rec.get('_id').generation_time
+            if datetime.now(timezone.utc) < dt + timedelta(days=7):  # existing record less than a week old? if so, ignore it
+                print("Ignoring {} as record is less than a week old.".format(asx_code))
+                continue
+        resp = fetcher.get(url)
+        try:
+            d = resp.json()
+            d.update({ 'asx_code': asx_code })
+            if 'error_desc' in d:
+                print("WARNING: ignoring {} ASX code as it is not a valid code".format(asx_code))
+                print(d) 
+            else:
+                assert d.pop('code', None) == asx_code
+                db.asx_company_details.find_one_and_update({ 'asx_code': asx_code }, { '$set': d }, upsert=True)
+        except Exception as e:
+            print(str(e))
+            pass 
+        time.sleep(5)
+
 if __name__ == "__main__":
     args = argparse.ArgumentParser(description="Fetch and update data from ASX Research")
     args.add_argument('--config', help="Configuration file to use [config.json]", type=str, default="config.json")
     args.add_argument('--want-companies', help="Update companies list", action="store_true")
     args.add_argument('--want-isin', help="Update securities list", action="store_true")
+    args.add_argument('--want-prices', help="Update ASX stock price list with current data", action="store_true")
+    args.add_argument('--want-details', help="Update ASX company details (incl. dividend, annual report etc.) with current data", action="store_true")
     a = args.parse_args() 
  
     config = {} 
@@ -87,6 +172,12 @@ if __name__ == "__main__":
     if a.want_isin:
         print("**** UPDATING ASX SECURITIES")
         update_isin(db, config, ensure_indexes=True) 
-
+    if a.want_prices:
+        print("**** UPDATING PRICES")
+        update_prices(db, available_stocks(db), config, ensure_indexes=True)
+    if a.want_details:
+        print("**** UPDATING COMPANY DETAILS")
+        update_company_details(db, available_stocks(db), config, ensure_indexes=True)
+         
     mongo.close()
     exit(0)
