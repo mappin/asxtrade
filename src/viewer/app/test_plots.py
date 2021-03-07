8c6cc0af3e5a6245149a593ffba11d347ef0b7b2
Andrew Cassin <acassin999@gmail.com>
2021-03-07 16:29:16 +1100

    Fix unit tests

--- src/viewer/app/test_plots.py
+++ src/viewer/app/test_plots.py
@@ -6,4 +6,4 @@ def test_price_change_bins():
     assert all([bins is not None, labels is not None])
     assert len(bins) == len(labels) + 1
     assert labels == ['-100.0', '-10.0', '-5.0', '-3.0', '-2.0', '-1.0', '-1e-06',
-                      '0.0', '1e-06', '1.0', '2.0', '3.0', '5.0', '10.0', '100.0', '1000.0']
+                      '0.0', '1e-06', '1.0', '2.0', '3.0', '5.0', '10.0', '25.0', '100.0', '1000.0']
