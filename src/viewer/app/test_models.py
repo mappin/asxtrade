3a5bec6b984d0c42248eed111ea3070c479b0cd0
Andrew Cassin <acassin999@gmail.com>
2021-03-07 18:28:54 +1100

    Fix bug in eps_as_cents() where it would not always return float. Add some quotation unit tests

--- src/viewer/app/test_models.py
+++ src/viewer/app/test_models.py
@@ -1,5 +1,5 @@
 import pytest
-from app.models import validate_stock, validate_date, desired_dates
+from app.models import validate_stock, validate_date, desired_dates, Quotation
 from datetime import datetime
 
 def test_validate_stock():
@@ -22,3 +22,22 @@ def test_desired_dates():
     ret = desired_dates(start_date=7, today=now)
     assert ret == ['2020-08-17', '2020-08-18', '2020-08-19',
                    '2020-08-20', '2020-08-21', '2020-08-22', '2020-08-23']
+
+def test_quotation_error_handling():
+    q = Quotation(error_code='404', change_in_percent=4.4)
+    assert q.is_error()
+    assert q.volume_as_millions() == ""  # since is_error() is True
+
+def test_quotation_volume_handling():
+    q = Quotation(error_code='', volume=1000 * 1000, last_price=1.0)
+    assert q.volume_as_millions() == "1.00"
+    q = Quotation(error_code='', volume=10 * 1000 * 1000, last_price=1.0)
+    assert q.volume_as_millions() == "10.00"
+
+def test_quotation_eps_handling(): 
+    q = Quotation(error_code='', eps=3.36)
+    assert q.eps - 3.36 < 0.001
+    assert q.eps_as_cents() == 336.0
+    q = Quotation() # eps is None
+    assert q.eps is None
+    assert q.eps_as_cents() == 0.0
