6a6851d1f4eb5187483ebfc321e54a7815c5a844
Andrew Cassin <acassin999@gmail.com>
2021-03-08 19:56:22 +1100

    Avoid use of wildcard imports and other pylint improvements

--- src/viewer/app/urls.py
+++ src/viewer/app/urls.py
@@ -14,44 +14,96 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from app.views import *
 from django.conf import settings
+from django.urls import path, include
+from app.views import (
+    show_all_stocks,
+    sector_search,
+    dividend_search,
+    company_search,
+    mover_search,
+    show_increasing_eps_stocks,
+    show_increasing_yield_stocks,
+    show_trends,
+    show_purchase_performance,
+    show_watched,
+    show_etfs,
+    show_stock,
+    show_sector_outliers,
+    show_watchlist_outliers,
+    toggle_watched,
+    buy_virtual_stock,
+    edit_virtual_stock,
+    delete_virtual_stock,
+    market_sentiment,
+    download_data,
+    show_optimised_watchlist,
+    show_optimised_sector,
+    show_optimised_etfs,
+)
 
 urlpatterns = [
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('admin/', admin.site.urls),
-    path('', show_all_stocks),
-    path('search/by-sector', sector_search),
-    path('search/by-yield', dividend_search),
-    path('search/by-company', company_search),
-    path('search/movers', mover_search),
-    path('show/increasing-eps', show_increasing_eps_stocks),  # NB: order important here!
-    path('show/increasing-yield', show_increasing_yield_stocks),
-    path('show/trends', show_trends),
-    path('show/purchase-performance', show_purchase_performance),
-    path('show/watched', show_watched, name='show-watched'),
-    path('show/etfs', show_etfs, name='show-etfs'),
-    path('show/<str:stock>', show_stock, name='show-stock'),
-    path('show/outliers/sector/<int:sector_id>/<int:n_days>', show_sector_outliers, name='show-sector-outliers'),
-    path('show/outliers/watchlist/<int:n_days>', show_watchlist_outliers, name='show-watchlist-outliers'), # eg. show/outliers/{all,watchlist} WARNING: slow
-    path('watchlist/<str:stock>', toggle_watched),
-    path('purchase/<str:stock>', buy_virtual_stock),
-    path('update/purchase/<slug:slug>', edit_virtual_stock),
-    path('delete/purchase/<slug:slug>', delete_virtual_stock),
-    path('stats/market-sentiment', market_sentiment),
-    path('data/<slug:dataset>/<str:format>/', download_data, name='data'),
-    path('show/optimized/watchlist/', show_optimised_watchlist, name='show-optimised-watchlist'),
-    path('show/optimized/watchlist/<str:exclude>', show_optimised_watchlist, name='show-optimised-watchlist'),
-    path('show/optimized/sector/<int:sector_id>/', show_optimised_sector, name='show-optimised-sector'),
-    path('show/optimized/sector/<int:sector_id>/<str:exclude>', show_optimised_sector, name='show-optimised-sector'),
-    path('show/optimized/etfs/', show_optimised_etfs, name='show-optimised-etfs'),
-    path('show/optimized/etfs/<str:exclude>', show_optimised_etfs, name='show-optimised-etfs')
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("admin/", admin.site.urls),
+    path("", show_all_stocks),
+    path("search/by-sector", sector_search),
+    path("search/by-yield", dividend_search),
+    path("search/by-company", company_search),
+    path("search/movers", mover_search),
+    path(
+        "show/increasing-eps", show_increasing_eps_stocks
+    ),  # NB: order important here!
+    path("show/increasing-yield", show_increasing_yield_stocks),
+    path("show/trends", show_trends),
+    path("show/purchase-performance", show_purchase_performance),
+    path("show/watched", show_watched, name="show-watched"),
+    path("show/etfs", show_etfs, name="show-etfs"),
+    path("show/<str:stock>", show_stock, name="show-stock"),
+    path(
+        "show/outliers/sector/<int:sector_id>/<int:n_days>",
+        show_sector_outliers,
+        name="show-sector-outliers",
+    ),
+    path(
+        "show/outliers/watchlist/<int:n_days>",
+        show_watchlist_outliers,
+        name="show-watchlist-outliers",
+    ),  # eg. show/outliers/{all,watchlist} WARNING: slow
+    path("watchlist/<str:stock>", toggle_watched),
+    path("purchase/<str:stock>", buy_virtual_stock),
+    path("update/purchase/<slug:slug>", edit_virtual_stock),
+    path("delete/purchase/<slug:slug>", delete_virtual_stock),
+    path("stats/market-sentiment", market_sentiment),
+    path("data/<slug:dataset>/<str:format>/", download_data, name="data"),
+    path(
+        "show/optimized/watchlist/",
+        show_optimised_watchlist,
+        name="show-optimised-watchlist",
+    ),
+    path(
+        "show/optimized/watchlist/<str:exclude>",
+        show_optimised_watchlist,
+        name="show-optimised-watchlist",
+    ),
+    path(
+        "show/optimized/sector/<int:sector_id>/",
+        show_optimised_sector,
+        name="show-optimised-sector",
+    ),
+    path(
+        "show/optimized/sector/<int:sector_id>/<str:exclude>",
+        show_optimised_sector,
+        name="show-optimised-sector",
+    ),
+    path("show/optimized/etfs/", show_optimised_etfs, name="show-optimised-etfs"),
+    path(
+        "show/optimized/etfs/<str:exclude>",
+        show_optimised_etfs,
+        name="show-optimised-etfs",
+    ),
 ]
 
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+    urlpatterns = [path("__debug__/", include(debug_toolbar.urls)),] + urlpatterns
